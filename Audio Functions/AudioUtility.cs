75494b492608b4436ee285a81fee76152642de6f
xvelastin <80051955+xvelastin@users.noreply.github.com>
2021-10-07 08:56:38 +0100

    Update AudioUtility.cs

--- Audio Functions/AudioUtility.cs
+++ Audio Functions/AudioUtility.cs
@@ -41,6 +41,11 @@ public static class AudioUtility
         return (float)(newStart + ((value - originalStart) * scale));
     }
 
+    /// <summary>
+    /// Gets the Audio Mixer Group of a given string name. Note this relies on the main mixer being called "Master".
+    /// </summary>
+    /// <param name="groupName">The name of the Audio Mixer Group to be returned.</param>
+    /// <returns>The Audio Mixer Group.</returns>
     public static AudioMixerGroup GetMixerGroup(string groupName)
     {
         AudioMixer masterMixer = Resources.Load("Master") as AudioMixer;
@@ -48,38 +53,46 @@ public static class AudioUtility
         return mixerGroup;
     }
 
-    private static IEnumerator FadeAudioSource(AudioSource source, float fadetime, float targetVol, float curveShape, bool stopAfterFade)
+    /// <summary>
+    /// Interpolates between the volume of an Audio Source and a target volume over a given duration with a configurable fade curve. It is almost identical to the full implementation in the Audio Source Controller script - however, unlike that script, it will not cancel a previous fade, so overlaps will happen unless handled correctly.
+    /// </summary>
+    /// <param name="source">The Audio Source to be faded.</param>
+    /// <param name="targetVolume">The volume in decibels that the Audio Source will be at at the end of the fade time.</param>
+    /// <param name="fadeTime">The time in seconds for the Audio Source to reach the target volume.</param>   
+    /// <param name="curveShape">Defines the bend of the fade curve, ie. the rate of change of the volume over time, from exponential (0) to s-curve (0.5) to logarithmic (1).</param>
+    /// <param name="stopAfterFade">If true, stops the Audio Source at the end of the fade.</param>
+    private static IEnumerator FadeAudioSource(AudioSource source, float targetVolume, float fadeTime, float curveShape, bool stopAfterFade)
     {
-        Keyframe[] keys = new Keyframe[2];
-        keys[0] = new Keyframe(0, 0, 0, 1f - curveShape, 0, 1f - curveShape);
-        keys[1] = new Keyframe(1, 1, 1f - curveShape, 0f, curveShape, 0);
-        AnimationCurve animcur = new AnimationCurve(keys);
-
-        if (source.gameObject.GetComponent<AudioSource>())
+        curveShape = Mathf.Clamp(curveShape, 0.0f, 1.0f);
+        if (fadeTime <= 0.0f)
         {
- source = source.gameObject.GetComponent<AudioSource>();
+            source.volume = targetVolume;
+            yield break;
         }
 
-        float startVol = ConvertAtoDb(source.volume);
-        float currentFadeVolume = startVol;
-        float currentTime = 0f;
+        // Creates an Animation Curve with Curve Shape to evaluate the fade over time.
+        Keyframe[] keys = new Keyframe[2];
+        keys[0] = new Keyframe(0, 0, 0, Mathf.Sin(curveShape), 0, 1.0f - curveShape);
+        keys[1] = new Keyframe(1, 1, 1 - curveShape, 0, curveShape, 0);
+        AnimationCurve fadeCurve = new AnimationCurve(keys);
 
-        while (currentTime < fadetime)
+        float startingVolume = ConvertAmplitudetoDb(source.volume);
+        float currentFadeVolume = startingVolume;
+        float currentTime = 0f;
+        while (currentTime < fadeTime)
         {
             currentTime += Time.deltaTime;
-            currentFadeVolume = Mathf.Lerp(startVol, targetVol, animcur.Evaluate(currentTime / fadetime));
+            currentFadeVolume = Mathf.Lerp(startingVolume, targetVolume, fadeCurve.Evaluate(currentTime / fadeTime));
             source.volume = ConvertDbtoAmplitude(currentFadeVolume);
             yield return null;
         }
 
         if (stopAfterFade)
         {
-            yield return new WaitForSeconds(fadetime);
+            yield return new WaitForSeconds(fadeTime);
             source.Stop();
         }
 
         yield break;
     }
-
 }
-
