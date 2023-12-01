6d32016d31edf2134a82c1c37e18bb5aa7496d12
xv <xaviervelastin@hotmail.com>
2023-12-01 12:26:11 +0000

    Update Audio Utility

--- AudioUtility/AudioUtility.cs
+++ AudioUtility/AudioUtility.cs
@@ -9,19 +9,24 @@ using UnityEngine.Audio;
 public static class AudioUtility
 {
     /// <summary>
-    /// Returns the universal minimum db value.
+    /// Returns the universal minimum volume value in decibels.
     /// </summary>
-    public static float minimum = -70.0f;
+    public static float MINIMUM_VOLUME = -60.0f;
+    
+    /// <summary>
+    /// Returns the universal maximum volume value in decibels.
+    /// </summary>
+    public static float MAXIMUM_VOLUME = 0.0f;
 
     /// <summary>
     /// Converts amplitude (0-1) as used in native Unity to the logarithmic decibel scale.
     /// </summary>
     /// <param name="amplitude"></param>
     /// <returns></returns>
-    public static float ConvertAmplitudetoDb(float amplitude)
+    public static float ToDecibels(float amplitude)
     {
-        amplitude = Mathf.Clamp(amplitude, ConvertDbtoAmplitude(minimum), 1f);
-        return 20 * Mathf.Log(amplitude) / Mathf.Log(10);
+        amplitude = Mathf.Clamp(amplitude, ToAmplitude(MINIMUM_VOLUME), 1f);
+        return Mathf.Clamp(20 * Mathf.Log(amplitude) / Mathf.Log(10), MINIMUM_VOLUME, MAXIMUM_VOLUME);
     }
 
     /// <summary>
@@ -29,39 +34,41 @@ public static class AudioUtility
     /// </summary>
     /// <param name="decibels"></param>
     /// <returns></returns>
-    public static float ConvertDbtoAmplitude(float decibels)
+    public static float ToAmplitude(float decibels)
     {
-        return Mathf.Pow(10, decibels / 20);
+        return decibels > MINIMUM_VOLUME ? Mathf.Pow(10, decibels / 20) : 0;
     }
+    
+    /// <summary>
+    /// Converts playback speed to semitones.
+    /// </summary>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public static float ToSemitones(float speed) => 12 * Mathf.Log(speed, 2);
+    
+    /// <summary>
+    /// Converts semitones to playback speed ie. the pitch value in Unity's Audio Source component.
+    /// </summary>
+    /// <param name="semitones"></param>
+    /// <returns></returns>
+    public static float ToPitch(float semitones) => Mathf.Pow(2, semitones / 12);
 
     public static float MapToRange(float value, float originalStart, float originalEnd, float newStart, float newEnd)
     {
-        // credit to Wim Coenen, https://stackoverflow.com/questions/4229662/convert-numbers-within-a-range-to-numbers-within-another-range //
+        // credit to Wim Coenen //
         double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
         return (float)(newStart + ((value - originalStart) * scale));
     }
 
     /// <summary>
-    /// Gets the Audio Mixer Group of a given string name. Note this relies on the main mixer being called "Master".
-    /// </summary>
-    /// <param name="groupName">The name of the Audio Mixer Group to be returned.</param>
-    /// <returns>The Audio Mixer Group.</returns>
-    public static AudioMixerGroup GetMixerGroup(string groupName)
-    {
-        AudioMixer masterMixer = Resources.Load("Master") as AudioMixer;
-        AudioMixerGroup mixerGroup = masterMixer.FindMatchingGroups(groupName)[0];
-        return mixerGroup;
-    }
-
-    /// <summary>
-    /// Interpolates between the volume of an Audio Source and a target volume over a given duration with a configurable fade curve. It is almost identical to the full implementation in the Audio Source Controller script - however, unlike that script, it will not cancel a previous fade, so overlaps will happen unless handled correctly.
+    /// Interpolates between the volume of an Audio Source and a target volume over a given duration with a configurable fade curve. Remember to cancel any ongoing fades when triggering.
     /// </summary>
     /// <param name="source">The Audio Source to be faded.</param>
     /// <param name="targetVolume">The volume in decibels that the Audio Source will be at at the end of the fade time.</param>
     /// <param name="fadeTime">The time in seconds for the Audio Source to reach the target volume.</param>   
     /// <param name="curveShape">Defines the bend of the fade curve, ie. the rate of change of the volume over time, from exponential (0) to s-curve (0.5) to logarithmic (1).</param>
     /// <param name="stopAfterFade">If true, stops the Audio Source at the end of the fade.</param>
-    private static IEnumerator FadeAudioSource(AudioSource source, float targetVolume, float fadeTime, float curveShape, bool stopAfterFade)
+    private static IEnumerator FadeAudioSource(AudioSource source, float targetVolume, float fadeTime, float curveShape = 0.5f, bool stopAfterFade = false)
     {
         curveShape = Mathf.Clamp(curveShape, 0.0f, 1.0f);
         if (fadeTime <= 0.0f)
@@ -76,14 +83,14 @@ public static class AudioUtility
         keys[1] = new Keyframe(1, 1, 1 - curveShape, 0, curveShape, 0);
         AnimationCurve fadeCurve = new AnimationCurve(keys);
 
-        float startingVolume = ConvertAmplitudetoDb(source.volume);
+        float startingVolume = ToDecibels(source.volume);
         float currentFadeVolume = startingVolume;
         float currentTime = 0f;
         while (currentTime < fadeTime)
         {
             currentTime += Time.deltaTime;
             currentFadeVolume = Mathf.Lerp(startingVolume, targetVolume, fadeCurve.Evaluate(currentTime / fadeTime));
-            source.volume = ConvertDbtoAmplitude(currentFadeVolume);
+            source.volume = ToAmplitude(currentFadeVolume);
             yield return null;
         }
 
